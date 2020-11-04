ce58f9e18430aa510241fd88b7ce668d37abd467
kkylelol <klye290199@gmail.com>
2020-11-04 13:10:34 +0800

    添加项目文件。

---
+++ login.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CWork
+{
+    partial class login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginID_tbx = new System.Windows.Forms.TextBox();
+            this.LoginPWD_tbx = new System.Windows.Forms.TextBox();
+            this.LoginID_lab = new System.Windows.Forms.Label();
+            this.LoginPWD_lab = new System.Windows.Forms.Label();
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginID_tbx
+            // 
+            this.LoginID_tbx.Location = new System.Drawing.Point(138, 78);
+            this.LoginID_tbx.Name = "LoginID_tbx";
+            this.LoginID_tbx.Size = new System.Drawing.Size(100, 21);
+            this.LoginID_tbx.TabIndex = 0;
+            // 
+            // LoginPWD_tbx
+            // 
+            this.LoginPWD_tbx.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.LoginPWD_tbx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LoginPWD_tbx.Enabled = false;
+            this.LoginPWD_tbx.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LoginPWD_tbx.Location = new System.Drawing.Point(138, 120);
+            this.LoginPWD_tbx.Name = "LoginPWD_tbx";
+            this.LoginPWD_tbx.Size = new System.Drawing.Size(100, 21);
+            this.LoginPWD_tbx.TabIndex = 1;
+            this.LoginPWD_tbx.UseSystemPasswordChar = true;
+            // 
+            // LoginID_lab
+            // 
+            this.LoginID_lab.AutoSize = true;
+            this.LoginID_lab.Location = new System.Drawing.Point(91, 81);
+            this.LoginID_lab.Name = "LoginID_lab";
+            this.LoginID_lab.Size = new System.Drawing.Size(41, 12);
+            this.LoginID_lab.TabIndex = 2;
+            this.LoginID_lab.Text = "账号：";
+            // 
+            // LoginPWD_lab
+            // 
+            this.LoginPWD_lab.AutoSize = true;
+            this.LoginPWD_lab.Location = new System.Drawing.Point(91, 129);
+            this.LoginPWD_lab.Name = "LoginPWD_lab";
+            this.LoginPWD_lab.Size = new System.Drawing.Size(41, 12);
+            this.LoginPWD_lab.TabIndex = 3;
+            this.LoginPWD_lab.Text = "密码：";
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.Location = new System.Drawing.Point(212, 238);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(75, 23);
+            this.Login_btn.TabIndex = 4;
+            this.Login_btn.Text = "登陆";
+            this.Login_btn.UseVisualStyleBackColor = true;
+            this.Login_btn.Click += new System.EventHandler(this.Login_btn_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Login_btn);
+            this.Controls.Add(this.LoginPWD_lab);
+            this.Controls.Add(this.LoginID_lab);
+            this.Controls.Add(this.LoginPWD_tbx);
+            this.Controls.Add(this.LoginID_tbx);
+            this.Name = "login";
+            this.Text = "登陆";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginID_tbx;
+        private System.Windows.Forms.TextBox LoginPWD_tbx;
+        private System.Windows.Forms.Label LoginID_lab;
+        private System.Windows.Forms.Label LoginPWD_lab;
+        private System.Windows.Forms.Button Login_btn;
+    }
+}
+
---
+++ login.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CWork
+{
+    public partial class login : Form
+    {                //连接数据库语句
+        SqlConnection cWork = new SqlConnection("Data Source=.;Initial Catalog=C#WORK;Integrated Security=True");
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_btn_Click(object sender, EventArgs e)
+        {
+            //获取输入内容
+            String loginid = LoginID_tbx.Text;
+            String loginpwd = LoginPWD_tbx.Text;
+            try
+            {
+                if (loginid.Trim() == "")
+                {
+                    MessageBox.Show("账号不能为空！");
+                    return;
+                }
+                else
+                {
+                    if (loginpwd.Trim() == "")
+
+                    {
+                        MessageBox.Show("密码不能为空！");
+                        return;
+                    }
+
+                }
+                //打开数据库
+                cWork.Open();
+                string loginSql = "select Sno from Student where Sno = " + loginid.Trim();
+                SqlCommand login = new SqlCommand(loginSql, cWork);
+                SqlDataReader loginred = login.ExecuteReader();
+
+                if (!loginred.Read())
+                {
+                    MessageBox.Show("账号或密码输入错误！");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("登录成功！");
+                    this.Close();
+                    
+                }
+
+            }
+            finally 
+            {//关闭数据库
+                cWork.Close();
+            }
+
+
+        }
+    }
+}
